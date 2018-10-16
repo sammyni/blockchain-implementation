760fdc7a2bac8d7b1f8562b946479b99f5f643b8
¨Samuel <samniwebdev@gmail.com>
2018-10-16 08:43:35 +0100

    Proof-of-work added

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,5 @@
 const  Block = require('./block');
+const { DIFFICULTY } = require('../config');
 
 describe('Block', () => {
     let data, lastBlock, block;
@@ -11,7 +12,13 @@ describe('Block', () => {
     it('sets the `data` to match the input', () => {
         expect(block.data).toEqual(data);
     });
+
     it('sets the `lastHash` to match the hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+        console.log('Mined Block', block.toString());
     });
 });
