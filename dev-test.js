9d41bdbf49ca110afa9eb39cfb45c23bf35a5e1b
¨Samuel <samniwebdev@gmail.com>
2018-10-17 00:28:50 +0100

    Added wallets and transactions

--- dev-test.js
+++ dev-test.js
@@ -1,6 +1,13 @@
-const Blockchain = require('./blockchain');
-const bc = new Blockchain();
+//Blockchain Dev Test
+// const Blockchain = require('./blockchain');
+// const bc = new Blockchain();
+//
+// for (let i = 0; i < 10; i++) {
+//     console.log(bc.addBlock(`foo ${i}`).toString());
+// }
 
-for (let i = 0; i < 10; i++) {
-    console.log(bc.addBlock(`foo ${i}`).toString());
-}
+
+//Wallet Dev Test
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.toString());
